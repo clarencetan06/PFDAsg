4d31efaf50bbf978ddb2054624cdc8ac36ba2e31
Clarence Tan <S10241977@connect.np.edu.sg>
2023-10-31 00:11:38 +0800

    Add project files.

---
+++ PFD_Assignment/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PFD_Assignment.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
